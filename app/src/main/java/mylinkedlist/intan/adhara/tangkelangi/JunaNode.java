91bf8b27848c620b66e9087be7cb2649c36f7289
Intan Adhara TAngkelangi <itangkelangi@student.ciputra.ac.id>
2026-04-06 12:46:54 +0800

    first commit

---
+++ app/src/main/java/mylinkedlist/intan/adhara/tangkelangi/JunaNode.java
@@ -0,0 +1,10 @@
+package mylinkedlist.intan.adhara.tangkelangi;
+
+public class JunaNode {
+    int data;
+    JunaNode next;
+    public JunaNode(int data){
+        this.data = data;
+        this.next = null;
+    }
+}
